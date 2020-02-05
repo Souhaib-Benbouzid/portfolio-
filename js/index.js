39c5281e7cd1cd7ab107c7e5b93ef9d419b154c9
Sohaib-Benbouzid <sohaib.code@gmail.com>
2020-02-05 14:39:17 +0100

    finish the responsive part

--- js/index.js
+++ js/index.js
@@ -4,22 +4,9 @@ const menuBtn = document.querySelector('.menu-btn');
 
 const nav = document.querySelector('.navbar');
 const menuNav = document.querySelector('.menu-nav');
-const links = document.querySelectorAll('.menu-link');
 
 let menuOpen = false;
-links.forEach().(addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        menuOpen = false;
-    }
-}))
+
 
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
@@ -34,4 +21,37 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
     
-})
+});
+
+
+
+const parent = document.querySelector(".menu-nav");
+parent.addEventListener('click', addOpen, false);
+
+function addOpen(e) {
+    if (e.target !== e.currentTarget){
+        if(!menuOpen) {
+            menuBtn.classList.add('open');
+            nav.classList.add('open');
+            menuNav.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            nav.classList.remove('open');
+            menuNav.classList.remove('open');
+            menuOpen = false;
+        }
+    }
+    e.stopPropagation;
+}
+
+
+
+
+
+
+
+
+
+
+
